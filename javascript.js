36e6cd4d5bcedef11fdc1edf4faa398e6c8a8e92
Caio Aguiar <88971985+CaioAguiar1@users.noreply.github.com>
2022-03-28 09:08:37 -0300

    halo2022

---
+++ javascript.js
@@ -0,0 +1,6 @@
+function togglemenu(){
+    var menuToggle = document.querySelector('.toggle');
+    var banner = document.querySelector('.banner');
+    menuToggle.classlist.toggle('active');
+    banner.classlist.toggle('active');
+    }
